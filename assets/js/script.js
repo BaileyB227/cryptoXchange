abea4828e3c4a5b353a94faf0e5a56e76cd13a40
Jeffrey McLeod <jeffreymmcleod@outlook.com>
2021-08-16 12:00:17 -0500

    added api keys for currency exchange, and crypto exchange

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+let apiKeyCurrency = "4b83b17cefa1b951b3da100dbb82ba7e";
+let apiKeyCrypto = 'http://data.fixer.io/api/latest?access_key=${apiKeyCurrency}'
+let apiKeyCurrencyURL = 'ge60haz3waeycxlzu7wnki'
+let apiKeyCryptoURL = 'https://api.lunarcrush.com/v2?data=assets&key=${apiKeyCrypto}&symbol=BTC'
