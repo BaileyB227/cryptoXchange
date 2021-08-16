53bfcfdb59918cba626379dfc0d85da11af02b52
Jeffrey McLeod <jeffreymmcleod@outlook.com>
2021-08-16 14:00:20 -0500

    fixing js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+  
+let apiKeyCurrency = '4b83b17cefa1b951b3da100dbb82ba7e';
+let apiKeyCurrencyURL = `http://data.fixer.io/api/latest?access_key=${apiKeyCurrency}`
+let apiKeyCrypto = 'fregfzsfixaw8mmf6bd7';
+let apiKeyCryptoURL = `https://api.lunarcrush.com/v2?data=assets&key=${apiKeyCrypto}`
+
 let apiFetch = 'http://data.fixer.io/api/latest?access_key=4b83b17cefa1b951b3da100dbb82ba7e'
 let apiFetch2 = 'https://api.lunarcrush.com/v2?data=assets&key=fregfzsfixaw8mmf6bd7&symbol=DOGE'
 
@@ -7,4 +13,4 @@ fetch (apiFetch)
 
 fetch (apiFetch2)
 .then(headers => headers.json())
-.then(data => {console.log(data)}) 
+.then(data => {console.log(data)})
