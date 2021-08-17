d90aa789635a95efb241d9886c0563c88b5d656c
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-08-17 13:34:15 -0500

    added javascript for currency, currently only works for EUR

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 let apiKeyCurrency = '4b83b17cefa1b951b3da100dbb82ba7e';
-let apiKeyCurrencyURL = `http://data.fixer.io/api/latest?access_key=${apiKeyCurrency}`
+let apiKeyCurrencyURL = `http://data.fixer.io/api/latest?access_key=${apiKeyCurrency}&base=USD`
 let apiKeyCrypto = 'fregfzsfixaw8mmf6bd7';
 let apiKeyCryptoURL = `https://api.lunarcrush.com/v2?data=assets&key=${apiKeyCrypto}`
 
-let apiFetch = 'http://data.fixer.io/api/latest?access_key=4b83b17cefa1b951b3da100dbb82ba7e'
-let apiFetch2 = 'https://api.lunarcrush.com/v2?data=assets&key=fregfzsfixaw8mmf6bd7&symbol=DOGE'
+let apiFetch = 'http://data.fixer.io/api/latest?access_key=4b83b17cefa1b951b3da100dbb82ba7e&base=USD'
+let apiFetch2 = 'https://api.lunarcrush.com/v2?data=meta&key=fregfzsfixaw8mmf6bd7&type=price&symbol=ETH'
 
 fetch (apiFetch)
 .then(headers => headers.json())
@@ -12,21 +12,43 @@ fetch (apiFetch)
 
 fetch (apiFetch2)
 .then(headers => headers.json())
-.then(data => {console.log(data)})
+.then(crypto => {console.log(crypto.data[0].price)})
 
 // Convert button, puts converted value in box and adds to local storage
 $("#convertBtn").on("click", function(){
     convertOptions();
-    storeConversion();
 })
 
 function convertOptions(){
     // Grabs the value of the selected options
     let $cryptoChoice = $('#cryptoOptions').val();
     let $currencyChoice = $('#currencyOptions').val();
+    let cryptoURL = `https://api.lunarcrush.com/v2?data=meta&key=${apiKeyCrypto}&type=price&symbol=${$cryptoChoice}`
+    let currencyURL = `https://currency-converter5.p.rapidapi.com/currency/convert`
 
-    fetch()
+    fetch(cryptoURL)
+    .then(headers => headers.json())
+    .then(cryptoData => {
+        let cryptoValue = cryptoData.data[0].price;
+        console.log(cryptoValue)
+        console.log($currencyChoice)
+        
+        fetch(`https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from=USD&to=${$currencyChoice}&amount=${cryptoValue}`, {
+	    "method": "GET",
+	    "headers": {
+		    "x-rapidapi-key": "50a769f7fdmshb1669138dd5c7a6p123efejsnef1425783bd8",
+		    "x-rapidapi-host": "currency-converter5.p.rapidapi.com"
+	        }
+        })
+        .then(headers => headers.json())
+        .then(response => {let responseRate = response.rates
 
-    console.log($cryptoChoice)
-    console.log($currencyChoice)
+            // Copy this one for each currency
+            if ($currencyChoice === "EUR") {
+                responseRate = responseRate.EUR.rate_for_amount
+                console.log(responseRate)
+            }
+        }
+            )
+    })
 }
