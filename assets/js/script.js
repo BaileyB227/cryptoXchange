ea0fb72e4cc3548af74163f5b3359f7e99297543
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-08-18 09:05:38 -0500

    fixed up javascript, added currency for conversions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,4 @@
-let apiKeyCurrency = '4b83b17cefa1b951b3da100dbb82ba7e';
-let apiKeyCurrencyURL = `http://data.fixer.io/api/latest?access_key=${apiKeyCurrency}&base=USD`
 let apiKeyCrypto = 'fregfzsfixaw8mmf6bd7';
-let apiKeyCryptoURL = `https://api.lunarcrush.com/v2?data=assets&key=${apiKeyCrypto}`
-
-let apiFetch = 'http://data.fixer.io/api/latest?access_key=4b83b17cefa1b951b3da100dbb82ba7e&base=USD'
-let apiFetch2 = 'https://api.lunarcrush.com/v2?data=meta&key=fregfzsfixaw8mmf6bd7&type=price&symbol=ETH'
-
-fetch (apiFetch)
-.then(headers => headers.json())
-.then(data => {console.log(data)})
-
-fetch (apiFetch2)
-.then(headers => headers.json())
-.then(crypto => {console.log(crypto.data[0].price)})
 
 // Convert button, puts converted value in box and adds to local storage
 $("#convertBtn").on("click", function(){
@@ -24,7 +10,6 @@ function convertOptions(){
     let $cryptoChoice = $('#cryptoOptions').val();
     let $currencyChoice = $('#currencyOptions').val();
     let cryptoURL = `https://api.lunarcrush.com/v2?data=meta&key=${apiKeyCrypto}&type=price&symbol=${$cryptoChoice}`
-    let currencyURL = `https://currency-converter5.p.rapidapi.com/currency/convert`
 
     fetch(cryptoURL)
     .then(headers => headers.json())
@@ -32,8 +17,9 @@ function convertOptions(){
         let cryptoValue = cryptoData.data[0].price;
         console.log(cryptoValue)
         console.log($currencyChoice)
+        let currencyURL = `https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from=USD&to=${$currencyChoice}&amount=${cryptoValue}`
         
-        fetch(`https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from=USD&to=${$currencyChoice}&amount=${cryptoValue}`, {
+        fetch(currencyURL, {
 	    "method": "GET",
 	    "headers": {
 		    "x-rapidapi-key": "50a769f7fdmshb1669138dd5c7a6p123efejsnef1425783bd8",
@@ -44,8 +30,44 @@ function convertOptions(){
         .then(response => {let responseRate = response.rates
 
             // Copy this one for each currency
+            if ($currencyChoice === "USD") {
+                responseRate = responseRate.USD.rate_for_amount
+                console.log(responseRate)
+            }
             if ($currencyChoice === "EUR") {
                 responseRate = responseRate.EUR.rate_for_amount
+                console.log(responseRate)
+            }
+            if ($currencyChoice === "JPY") {
+                responseRate = responseRate.JPY.rate_for_amount
+                console.log(responseRate)
+            }
+            if ($currencyChoice === "GBP") {
+                responseRate = responseRate.GBP.rate_for_amount
+                console.log(responseRate)
+            }
+            if ($currencyChoice === "AUD") {
+                responseRate = responseRate.AUD.rate_for_amount
+                console.log(responseRate)
+            }
+            if ($currencyChoice === "CAD") {
+                responseRate = responseRate.CAD.rate_for_amount
+                console.log(responseRate)
+            }
+            if ($currencyChoice === "CHF") {
+                responseRate = responseRate.CHF.rate_for_amount
+                console.log(responseRate)
+            }
+            if ($currencyChoice === "CNY") {
+                responseRate = responseRate.CNY.rate_for_amount
+                console.log(responseRate)
+            }
+            if ($currencyChoice === "HKD") {
+                responseRate = responseRate.HKD.rate_for_amount
+                console.log(responseRate)
+            }
+            if ($currencyChoice === "NZD") {
+                responseRate = responseRate.NZD.rate_for_amount
                 console.log(responseRate)
             }
         }
